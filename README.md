d4592e6d3b718994f8c5badc013c331018eec215
brenthanniman <brent@brents-imac.localdomain>
2016-01-31 00:35:42 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Getting and Cleaning Data Project
+
+This R Script: run_analysis.R generates a tidy data set from data collected from Samsung Galaxy S accelerometers.
+The original data can be found at the UCI Machine Learning Repository website: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+The raw data can be found at the following URL: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+The script follows the following steps to generate the tidy data set:
+
+1. Retrieves the data
+  + Downloads the dataset from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+  + Unzips the raw data
+
+2. Reads in the required activity and feature labels and stores them in vectors 
+  + Activity labels: ./UCI HAR Dataset/activity_labels.txt
+  + Feature labels: ./UCI HAR Dataset/features.txt
+
+3. Generates a data frame of the test measurements: test_set
+  + Loads the subject measurments from: ./UCI HAR Dataset/test/subject_test.txt
+  + Loads the activity reference indices for hte measurements from : ./UCI HAR Dataset/test/y_test.txt
+  + Loads the feature measurements from: ./UCI HAR Dataset/test/X_test.txt
+  + Column binds the individual vectors into one data frame
+
+4. Generates a data frame of the training measurements: train_set
+  + Loads the subject measurments from: ./UCI HAR Dataset/train/subject_train.txt
+  + Loads the activity reference indices for hte measurements from : ./UCI HAR Dataset/train/y_train.txt
+  + Loads the feature measurements from: ./UCI HAR Dataset/train/X_train.txt
+  + Column binds the individual vectors into one data frame
+
+5. Merge the test and training sets together into one data frame: complete_set
+
+6. Subset the data frame for just mean and standard deviation measurements: subset
+  + Selected all features whose labels contained mean or std
+
+7. Replace the numerical activity indices in the ativity variable with the decriptive activity labels from activity_labels.txt
+
+8. Stored the resulting tidy data in a new data frame: tidy_dataset_mean_std
+
+9. Generated a new data set with the average of each variable for each activity and each subject: tidy_dataset_mean_std_averages
---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+## Getting and Cleaning Data - Course Project
+
+## This R Script generates a tidy data set from data collected from Samsung Galaxy S accelerometers
+## The original data was obtained from: 
+## http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+## The raw data can be found at the following URL:
+## https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+##Check to see if the file to download has already been downloaded, if not, then download it
+if (!file.exists("Dataset.zip")) {
+  ## Download the file and unzip it
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
+                  destfile = "Dataset.zip", method = "curl") 
+  unzip("Dataset.zip", exdir=".")
+} else {
+  ## File has already been downloaded, let the user know you are using the stored copy
+  message("Data already downloaded. Using stored Copy")
+}  
+
+## Read-in activity labels and features
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", col.names = c("index","activity_label"))
+features <- read.table("./UCI HAR Dataset/features.txt", col.names = c("index","feature_name"))
+
+## Load test set and combine into 1 data frame
+test_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+test_activity <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "activity")
+test_readings <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = features$feature_name)
+test_set <- cbind(test_subject, test_activity, test_readings)
+
+## Load training set and combine into 1 data frame
+train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+train_activity <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "activity")
+train_readings <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = features$feature_name)
+train_set <- cbind(train_subject, train_activity, train_readings)
+
+## Merge the two tables together
+complete_set <- rbind(test_set, train_set)
+
+## Select only those features that represent mean and standard deviation
+mean_features <- grep("mean()", names(complete_set))
+std_features <- grep("std()", names(complete_set))
+
+## Extract only the measurements on mean and standard deviation of each measurement
+library(dplyr)
+subset <- complete_set %>%
+  select(subject, activity, mean_features, std_features)
+
+## Replace the activity indices with descriptive names
+for (i in 1:6) {subset$activity <- sub(activity_labels$index[i],activity_labels$activity_label[i], subset$activity)}
+
+## Give a meaningful name to the dataset
+tidy_dataset_mean_std <- subset
+
+## Provide an independet tidy data set with the average of each variable for each activity and each subject
+tidy_dataset_mean_std_averages <- tbl_df(tidy_dataset_mean_std)
+tidy_dataset_mean_std_averages <- tidy_dataset_mean_std_averages %>%
+  group_by(subject, activity) %>%
+  summarise_each(funs(mean))
